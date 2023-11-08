4cbcb7180a7691464355f308b581684d4f15651e
siu <francisco.rodriguez@iesnervion.es>
2023-11-08 10:19:07 +0100

    comentarios y a entregar

--- EjercicioFicheros02/src/ejercicios/Ejercicio.java
+++ EjercicioFicheros02/src/ejercicios/Ejercicio.java
@@ -9,23 +9,24 @@ public class Ejercicio {
 		File ficheroLeer = new File("palabras.txt");
 		
 		try {
-			new File("C:\\Users\\frodriguez\\palabrasSeparadas.txt").createNewFile();
-			FileReader fr = new FileReader(ficheroLeer);
-			BufferedReader br = new BufferedReader(fr);
-			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\frodriguez\\palabrasSeparadas.txt"));
-					
+			// new File("C:\\Users\\frodriguez\\palabrasSeparadas.txt").createNewFile(); Para crear un documento en tu ordenador
+			FileReader fr = new FileReader(ficheroLeer); //Creamos un filereader 
+			BufferedReader br = new BufferedReader(fr); //Creamos un bufferedreader para leer el fichero
+			BufferedWriter bw = new BufferedWriter(new FileWriter("palabrasSeparadas.txt")); //Creamos un bufferedwriter para escribir
+																							//en el nuevo fichero
+			//Leemos la primera línea
 			String linea = br.readLine();
-			List<String> palabras = encuentraMayus(linea);
-			for (String palabra:palabras) {
-				bw.write(palabra);
-				bw.write("\n");
+			List<String> palabras = encuentraMayus(linea); //Llamamos a una función que nos devuelve una lista con 
+														   //las palabras separadas
+			for (String palabra:palabras) { //Por cada palabra de la lista
+				bw.write(palabra + "\n"); //Escribimos la palabra con un espacio
 			}
 			
-			br.close();
+			br.close(); //Cerramos los bufferedReaders y Writers
 			bw.flush();
 			bw.close();
-			
-			System.out.println(palabras);
+		
+			//Excepciones
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -33,23 +34,28 @@ public class Ejercicio {
 		}
 	}
 	
+	/**
+	 * Función que separa las palabras de la línea leída
+	 * @param linea Recibe la línea que leemos del fichero
+	 * @return Devuelve una lista con las palabras separas
+	 */
 	private static List<String> encuentraMayus(String linea) {
-		List<String> palabras = new ArrayList<String>();
-		char[] letras = linea.toCharArray();
-		String palabra = "";
+		List<String> palabras = new ArrayList<String>(); //Creamos una lista de strings
+		char[] letras = linea.toCharArray(); //Creamos un array de chars a partir de la línea leída
+		String palabra = ""; //Palabra donde iremos guardando cada palabra que creemos
 		
-		for (int i = 0; i<letras.length; i++) {
-			if (Character.isUpperCase(letras[i])) {
-				if(!palabra.equals("")) {
-					palabras.add(palabra);
-					palabra = "";
+		for (int i = 0; i<letras.length; i++) { //Recorremos el array de chars
+			if (Character.isUpperCase(letras[i])) { //Si el caracter es mayúsculas
+				if(!palabra.equals("")) { //Si alguna palabra ha sido escrita
+					palabras.add(palabra); //Añadimos la palabra a la lista 
+					palabra = ""; //Reseteamos la palabra
 				}
-				palabra += letras[i];
-			} else {
-				palabra += letras[i];
+				palabra += letras[i]; //Añadimos la letra mayúscula a la palabra
+			} else { //Si es minúscula
+				palabra += letras[i]; //Añadimos la letra a la palabra
 			}
 		}
 		
-		return palabras;
+		return palabras; //Devolvemos la lista de palabras
 	}
 }
