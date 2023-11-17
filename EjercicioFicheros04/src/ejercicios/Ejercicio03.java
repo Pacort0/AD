a1a20bf0062b403ff5119cb637d93c8cfc337409
siu <francisco.rodriguez@iesnervion.es>
2023-11-17 20:50:05 +0100

    Ejercicio 3 acabado y comentado

--- EjercicioFicheros04/src/ejercicios/Ejercicio03.java
+++ EjercicioFicheros04/src/ejercicios/Ejercicio03.java
@@ -8,23 +8,23 @@ import java.io.RandomAccessFile;
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		int letraA = 97;
-		int letraE = 101;
-		int saltoLinea = 10;
-		int numero1 = 49;
-		int posicion = 1;
+		int letraA = 97; //letra A en ASCII
+		int letraE = 101; //letra E en ASCII
+		int saltoLinea = 10; //salto de linea en ASCII 
+		int numero1 = 49; //numero 1 en ASCII
 		try {
 			File ficheroOriginal = new File("FicheroNormal2.txt");
 			ficheroOriginal.createNewFile();
 			RandomAccessFile ralf = new RandomAccessFile(ficheroOriginal, "rw");
 			
-			for(int i = letraA; i<=letraE; i++) {
+			//Escribimos las letras con un número detrás
+			for(int i = letraA; i<=letraE; i++) { 
 				ralf.write(i);
 				ralf.write(numero1);
 				ralf.write(saltoLinea);
 				numero1++;
 			}
-			
+			//Escribimos el fichero en inverso
 			escribeInverso(ralf);
 			ralf.close();
 		} catch (FileNotFoundException e) {
@@ -33,26 +33,35 @@ public class Ejercicio03 {
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * Método que le da la vuelta al fichero original y lo escribe en el fichero nuevo
+	 * @param ralf RandomAccessFile con los datos del fichero original
+	 */
 	private static void escribeInverso(RandomAccessFile ralf) {
 		try {
 			File ficheroReverso = new File("FicheroReverso2.txt");
 			ficheroReverso.createNewFile();
 			RandomAccessFile ficheroInverso = new RandomAccessFile(ficheroReverso, "rw");
 			
-			int letra;
-			int posicionFicheroOG = (int)ralf.length()-4;
+			int letra; //Variable donde guardaremos cada letra
+			//Variable que usaremos para mover el puntero del fichero a donde nos convenga
+			//Inicializamos a la posición de la primera letra, o longitud del fichero menos el salto de línea y el número
+			int posicionFicheroOG = (int)ralf.length()-3; 
 
+			//Mientras el valor del puntero no sea inferior a 0
 			while(posicionFicheroOG >=0) {
-				ralf.seek(posicionFicheroOG);
-				letra = ralf.readByte();
+				ralf.seek(posicionFicheroOG); //Cambiamos el puntero del fichero original a la primea letra por el final
+				letra = ralf.readByte(); //Leemos la letra en esa posición
+				ficheroInverso.write(letra); //Escribimos la letra en el fichero nuevo
+				posicionFicheroOG++; //Aumentamos la posicion del puntero en un valor para acceder a la posicion del numero
+				letra = ralf.readByte(); //Leemos el numero
+				ficheroInverso.write(letra); //Escribimos el número
+				posicionFicheroOG++; //Aumentamos la posicion del puntero en un valor para acceder a la posicion del salto de linea
+				letra = ralf.readByte(); //Leemos y escribimos el salto de línea
 				ficheroInverso.write(letra);
-				posicionFicheroOG++;
-				letra = ralf.readByte();
-				ficheroInverso.write(letra);
-				posicionFicheroOG++;
-				posicionFicheroOG-=4;
+				posicionFicheroOG-=5; //Nos vamos a la siguiente letra del fichero
 			}
-			ficheroInverso.close();
+			ficheroInverso.close(); //Cerramos el fichero
 			
 		} catch (IOException e) {
 			e.printStackTrace();
